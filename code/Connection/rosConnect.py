7edff7dcaa92a31741fabecc1ec356a23e5a4095
Leondara <Leondara@oregonstate.edu>
2018-01-31 15:07:34 -0800

    Add files via upload

---
+++ code/Connection/rosConnect.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+import os
+import sys
+import roslaunch
+import subprocess
+import rosbag
+from std_msgs.msg import Int32, String
+
+path1 = "../wheelchair_description/launch/wheelchair_bringup.launch"
+path2 = "../wheelchair_description/launch/wheelchair_merger.launch"
+path3 = "../wheelchair_navigation/launch/move_base_local.launch"
+
+def rosStartup():
+	roscore = subprocess.Popen('roscore')
+
+def rosScriptsStart():
+	rospy.init_node('launchFiles', anonymous=True)
+	#rospy.on_shutdown(self.shutdown)
+
+	uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+	roslaunch.configure_logging(uuid)
+	launch1 = roslaunch.parent.ROSLaunchParent(uuid, [path1])
+	launch2 = roslaunch.parent.ROSLaunchParent(uuid, [path2])
+	launch3 = roslaunch.parent.ROSLaunchParent(uuid, [path3])
+
+	launch1.start()
+	launch2.start()
+	launch3.start()
+
+def rosScriptsEnd():
+	launch1.shutdown()
+	launch2.shutdown()
+	launch3.shutdown()
+
+def recordBag():
+	rosbag = subprocess.Popen(['rosbag', 'record', 'tf', 'scan_multi', 'pose_stamped'], preexec_fn=os.setsid)
+	
+def stopRecordBag():
+	rosbag.send_signal(subprocess.signal.SIGINT)
