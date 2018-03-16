053a1e8319940dbb1c863ccb5d18e4526162d693
Marie <bomberm@oregonstate.edu>
2018-03-16 10:40:32 -0700

    launchFiles now is fully fucntional

--- code/ROSHandling/launchFiles.py
+++ code/ROSHandling/launchFiles.py
@@ -3,14 +3,15 @@
 # Used in Run Test interface when 'initialize' button is pushed
 import subprocess
 
-scripts = ['wheelchair_description wheelchair_bringup.launch', 'wheelchair_description wheelchair_merger.launch', 'wheelchair_navigation move_base_local.launch']
+scripts = [['wheelchair_description', 'wheelchair_bringup.launch'], ['wheelchair_description', 'wheelchair_merger.launch'], ['wheelchair_navigation', 'move_base_local.launch']]
 
 store_pid = open("launch.pid", 'w')
 store_log = []
 error_log = []
 for script in scripts:
-	store_log.append(open(script+'.log', 'w'))
-	error_log.append(open(script+'Error.log', 'w'))
-	output = subprocess.Popen('roslaunch '+script, stdout=store_log[-1], stderr=error_log[-1]) 
+	store_log.append(open(script[0]+'.log', 'w'))
+	error_log.append(open(script[0]+'Error.log', 'w'))
+	command=["roslaunch"]+script
+	output = subprocess.Popen(command, stdout=store_log[-1], stderr=error_log[-1]) 
 	store_pid.write(str(output.pid))
 store_pid.close()
