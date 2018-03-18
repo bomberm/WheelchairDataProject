cccd465b719d8825be5c4498365dc690fe3c7f71
Hadi <htrarabi@gmail.com>
2018-03-18 12:15:59 -0700

    Updated launchFiles to use json file as input. Added dependencies and .json file for Chiron

--- code/ROSHandling/launchFiles.py
+++ code/ROSHandling/launchFiles.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python
 
 # Used in Run Test interface when 'initialize' button is pushed
-import subprocess
+import subprocess #run the launch routines
+import json #json handling
+from sys import argv #access args
+from pathlib2 import Path
 
-scripts = [['wheelchair_description', 'wheelchair_bringup.launch'], ['wheelchair_description', 'wheelchair_merger.launch'], ['wheelchair_navigation', 'move_base_local.launch']]
+if not len(argv) == 2:
+	raise IOError('Usage: '+argv[0]+' <test.json file>')
+
+checkIfExists = Path(argv[1])
+if checkIfExists.is_file:
+	testFile = checkIfExists.open()
+else:
+	raise IOError('Usage: '+argv[1]+'must be a .json file')	
+
+data = json.load(testFile)
+scripts = data['launch']
 
 store_pid = open("launch.pid", 'w')
 store_log = []
 error_log = []
 for script in scripts:
+	print script
 	store_log.append(open(script[0]+'.log', 'w'))
 	error_log.append(open(script[0]+'Error.log', 'w'))
 	command=["roslaunch"]+script
