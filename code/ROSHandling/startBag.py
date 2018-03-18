864404a88b65ed547954e7359efc4509196e0cb7
Hadi <htrarabi@gmail.com>
2018-03-18 15:27:53 -0700

    Updated startBag to general use case

--- code/ROSHandling/startBag.py
+++ code/ROSHandling/startBag.py
@@ -1,17 +1,35 @@
 #/usr/bin/python
 
+# Used in Run Test interface when 'start recording' is pushed.
 import subprocess
-import sys
+import json
+from sys import argv
+from pathlib2 import Path
 
 def get_pid(name):
-    return subprocess.check_output(["pidof",name])
+	while 1:
+		try:
+    			return subprocess.check_output(["pidof",name])
+		except:
+			pass
 
-if len(sys.argv) < 3:
-	print "Usage: "+sys.argv[0]+" <location> <topic(s)>"
-	exit(-1)	
+if not len(argv) == 3:
+	raise IOError('Usage: '+argv[0]+' <save_location> <test.json file>')
+
+checkIfExists = Path(argv[2])
+if checkIfExists.is_file:
+	testFile = checkIfExists.open()
+else:
+	raise IOError('Usage: '+argv[2]+' must be a .json file.')
+
+data = json.load(testFile)
+topics = data['topics']
+
 corelog = open("bag.log", 'w')
 process = ['rosbag', 'record', '-o']
-process = process + sys.argv[1:]
+process.append(argv[1])
+process = process+topics
+print str(process)
 output = subprocess.Popen(process, stdout=corelog)
 storePid = open('bag.pid', 'w')
 storePid.write(get_pid('record'))
--- code/ROSHandling/startup.py
+++ code/ROSHandling/startup.py
@@ -1,5 +1,4 @@
 #/usr/bin/python
-
 import subprocess
 
 corelog = open("core.log", 'w')
