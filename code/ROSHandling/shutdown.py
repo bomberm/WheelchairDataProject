cac018a119983dd5caebf9ac294c7cf782037add
Hadi <htrarabi@gmail.com>
2018-04-02 17:13:34 -0700

    Added handling if core is already running

--- code/ROSHandling/shutdown.py
+++ code/ROSHandling/shutdown.py
@@ -6,5 +6,5 @@ if len(sys.argv) == 2:
 	pid = open(sys.argv[1]+".pid", 'r')
 	for line in pid:
 		os.kill(int(line), signal.SIGINT)
-	pid_to_handle.close()
+	pid.close()
 	os.remove(sys.argv[1]+".pid")
--- code/ROSHandling/startup.py
+++ code/ROSHandling/startup.py
@@ -1,6 +1,9 @@
 #/usr/bin/python
 import subprocess
+import os.path
 
+if os.path.isfile('core.pid'):
+	exit(0)
 corelog = open("core.log", 'w')
 output = subprocess.Popen('roscore', stdout = corelog) 
 store_pid = open("core.pid", 'w')
